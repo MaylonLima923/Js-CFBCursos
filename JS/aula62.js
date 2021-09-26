718fbdcd44534ffeb89e2df0cb1b020fc2692bdc
Maylon Lima <maylonlima923@gmail.com>
2021-09-25 23:38:17 -0300

    Arquivos aula 62

---
+++ JS/aula62.js
@@ -0,0 +1,9 @@
+var minhaJanela;
+
+function abreJ() {
+    minhaJanela = window.open("aula62b.html", "_blank", "width=300, heigth=200, top=500, left=300");
+}
+
+function fechaJ() {
+    minhaJanela.close();
+}
